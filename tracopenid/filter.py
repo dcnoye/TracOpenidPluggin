a96a0a9f6e4f7f4ffcbd297d4f3ec7ef0832b987
DC <dcnoye@gmail.com>
2024-04-01 12:46:39 -0700

    chore: bug fix

--- tracopenid/filter.py
+++ tracopenid/filter.py
@@ -3,12 +3,13 @@ from trac.core import Component, implements
 from trac.web.api import IRequestFilter
 from trac.config import BoolOption
 from trac.perm import PermissionError
+import inspect  # Import the inspect module
 
 class Oauth2Filter(Component):
     """ A filter to redirect unauthenticated """
     implements(IRequestFilter)
 
-    redirect_login = BoolOption('permredirect', 'redirect_login', 'true',""" """)
+    redirect_login = BoolOption('permredirect', 'redirect_login', 'true', """ """)
 
     def pre_process_request(self, req, handler):
         return handler
@@ -21,9 +22,11 @@ class Oauth2Filter(Component):
             return template, data, content_type
 
         exctype, exc = sys.exc_info()[0:2]
-        login_url = req.href.login()
-        if issubclass(exctype, PermissionError):
+
+        # Check if exctype is a class and is a subclass of PermissionError
+        if exctype is not None and inspect.isclass(exctype) and issubclass(exctype, PermissionError):
+            login_url = req.href.login()
             req.redirect(login_url)
 
-
         return template, data, content_type
+
